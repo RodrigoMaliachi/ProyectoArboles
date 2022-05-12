62efae5d0aefdcd4648e624b54df351c0479f19d
Rodrigo Maliachi <roderykmaliachi@gmail.com>
2022-05-12 13:28:19 -0500

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.proyectoarboles.proyectoarboles {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.proyectoarboles.proyectoarboles to javafx.fxml;
+    exports com.proyectoarboles.proyectoarboles;
+}
