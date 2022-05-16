ce6a485c236ac28cf78c79c25a5b44388f047c9c
Alma Ordoñez <alma.ordonez2002@gmail.com>
2022-05-16 16:01:59 -0500

    Clase arbol profesiones (prueba)
    No muestra todos los índices

--- src/main/java/modelo/Test.java
+++ src/main/java/modelo/Test.java
@@ -9,20 +9,25 @@ public class Test {
     public static void main(String[] args) throws ArbolException, SerializadorException {
 
         ArbolCalificacion arbolCalificacion;
+        ArbolProfesiones arbolProfesiones;
 
         if ( fileExists() ) {
-            arbolCalificacion = new ArbolCalificacion();
+            //arbolCalificacion = new ArbolCalificacion();
+            arbolProfesiones = new ArbolProfesiones();
         } else {
-            arbolCalificacion = new ArbolCalificacion();
-            arbolCalificacion.crearArbol(Archivo.leerArchivoCSV());
+            //arbolCalificacion = new ArbolCalificacion();
+            //arbolCalificacion.crearArbol(Archivo.leerArchivoCSV());
+            arbolProfesiones = new ArbolProfesiones();
+            arbolProfesiones.crearArbol(Archivo.leerArchivoCSV());
         }
 
-        arbolCalificacion.listar();
+        //arbolCalificacion.listar();
+        arbolProfesiones.listar();
     }
 
     private static boolean fileExists(){
-        File arb = new File("src/", "caliiones.arb");
-        File dat = new File("src/", "caliiones.dat");
+        File arb = new File("src/", "profesiones.arb");
+        File dat = new File("src/", "profesiones.dat");
 
         if ( !arb.exists() || !dat.exists() ) {
             arb.delete();
